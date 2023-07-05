8f371897f816336ed3601595928f56613712b3d5
grgzjs <julio@gustosogroup.com>
2023-07-05 18:02:40 -0400

    calendar_query file created

--- assets/js/app-pages-calendar.js
+++ assets/js/app-pages-calendar.js
@@ -35,13 +35,35 @@ var App = (function () {
     /* initialize the calendar
     -----------------------------------------------------------------*/
 
+    
+    let response = ""
+
+    let xhttp;
+    if (str == "") {
+      response = "";
+      return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        response = this.responseText;
+      }
+    };
+    xhttp.open("GET", "calendar_query.php?q="+str, true);
+    xhttp.send();
+
+    console.log(response);
+
+    let d = new Date();
+    let today = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
+
     $('#calendar').fullCalendar({
       header: {
         left: 'title',
         center: '',
         right: 'month,agendaWeek,agendaDay, today, prev,next',
       },
-      defaultDate: '2017-02-12',
+      defaultDate: today,
       editable: true,
       eventLimit: true,
       droppable: true, // this allows things to be dropped onto the calendar
@@ -53,60 +75,60 @@ var App = (function () {
         }
       },
       events: [
-        {
-          title: 'All Day Event',
-          start: '2017-02-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2017-02-07',
-          end: '2017-02-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-02-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-02-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2017-02-11',
-          end: '2017-02-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2017-02-12T10:30:00',
-          end: '2017-02-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2017-02-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2017-02-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2017-02-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2017-02-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2017-02-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2017-02-28'
-        }
+        // {
+        //   title: 'All Day Event',
+        //   start: '2017-02-01'
+        // },
+        // {
+        //   title: 'Long Event',
+        //   start: '2017-02-07',
+        //   end: '2017-02-10'
+        // },
+        // {
+        //   id: 999,
+        //   title: 'Repeating Event',
+        //   start: '2017-02-09T16:00:00'
+        // },
+        // {
+        //   id: 999,
+        //   title: 'Repeating Event',
+        //   start: '2017-02-16T16:00:00'
+        // },
+        // {
+        //   title: 'Conference',
+        //   start: '2017-02-11',
+        //   end: '2017-02-13'
+        // },
+        // {
+        //   title: 'Meeting',
+        //   start: '2017-02-12T10:30:00',
+        //   end: '2017-02-12T12:30:00'
+        // },
+        // {
+        //   title: 'Lunch',
+        //   start: '2017-02-12T12:00:00'
+        // },
+        // {
+        //   title: 'Meeting',
+        //   start: '2017-02-12T14:30:00'
+        // },
+        // {
+        //   title: 'Happy Hour',
+        //   start: '2017-02-12T17:30:00'
+        // },
+        // {
+        //   title: 'Dinner',
+        //   start: '2017-02-12T20:00:00'
+        // },
+        // {
+        //   title: 'Birthday Party',
+        //   start: '2017-02-13T07:00:00'
+        // },
+        // {
+        //   title: 'Click for Google',
+        //   url: 'http://google.com/',
+        //   start: '2017-02-28'
+        // }
       ]
     });
 
--- calendar.php
+++ calendar.php
@@ -16,6 +16,11 @@ include("conexion.php");
   <link rel="stylesheet" type="text/css" href="assets/lib/select2/css/select2.min.css" />
   <link rel="stylesheet" type="text/css" href="assets/lib/bootstrap-slider/css/bootstrap-slider.min.css" />
   <link rel="stylesheet" type="text/css" href="assets/lib/datepicker/css/bootstrap-datepicker3.min.css" />
+
+  <link rel="stylesheet" type="text/css" href="assets/lib/stroke-7/style.css"/>
+  <link rel="stylesheet" type="text/css" href="assets/lib/perfect-scrollbar/css/perfect-scrollbar.css"/>
+  <link rel="stylesheet" type="text/css" href="assets/lib/jquery.fullcalendar/fullcalendar.min.css"/>
+
   <link rel="stylesheet" href="assets/css/app.css" type="text/css" />
 </head>
 
@@ -96,7 +101,7 @@ include("conexion.php");
 
   <!--COMIENZO BOTONERA PRINCIPAL-->
 
-  <div class="mai-wrapper">
+  <div class="main-wrapper">
     <nav class="navbar navbar-expand-lg mai-sub-header">
       <div class="container">
         <nav class="navbar navbar-expand-md">
@@ -164,56 +169,53 @@ include("conexion.php");
 
 
     <div class="main-content container">
-        <div class="row full-calendar">
-          <div class="col-md-9">
-            <div class="card card-default card-fullcalendar">
-              <div class="card-body">
-                <div id="calendar"></div>
-              </div>
+      <div class="row full-calendar">
+        <div class="col-md-9">
+          <div class="card card-default card-fullcalendar">
+            <div class="card-body">
+              <div id="calendar"></div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="card card-default fullcalendar-external-events">
-              <div class="card-header card-header-divider">Draggable Events</div>
-              <div class="card-body">
-                <div id="external-events">
-                  <div class="fc-event">My Event 1</div>
-                  <div class="fc-event">My Event 2</div>
-                  <div class="fc-event">My Event 3</div>
-                  <div class="fc-event">My Event 4</div>
-                  <div class="fc-event">My Event 5</div>
-                  <!-- <p>
+        </div>
+        <div class="col-md-3">
+          <div class="card card-default fullcalendar-external-events">
+            <div class="card-header card-header-divider">Draggable Events</div>
+            <div class="card-body">
+              <div id="external-events">
+                <div class="fc-event">My Event 1</div>
+                <div class="fc-event">My Event 2</div>
+                <div class="fc-event">My Event 3</div>
+                <div class="fc-event">My Event 4</div>
+                <div class="fc-event">My Event 5</div>
+                <!-- <p>
                     <input id="drop-remove" type="checkbox">
                     <label for="drop-remove">remove after drop</label>
                   </p> -->
-                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
 
 
   </div>
-  </div>
-  </div>
-    <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
-    <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript"></script>
-    <script src="assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-    <script src="assets/js/app.js" type="text/javascript"></script>
-    <script src="assets/lib/moment.js/min/moment.min.js" type="text/javascript"></script>
-    <script src="assets/lib/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="assets/lib/jquery.fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
-    <script src="assets/js/app-pages-calendar.js" type="text/javascript"></script>
-    <script type="text/javascript">
-      $(document).ready(function(){
-      	//-initialize the javascript
-      	App.init();
-      	App.pageCalendar();
-      });
-    </script>
-  <script>
-
+  <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript"></script>
+  <script src="assets/lib/bootstrap/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+  <script src="assets/js/app.js" type="text/javascript"></script>
+  <script src="assets/lib/moment.js/min/moment.min.js" type="text/javascript"></script>
+  <script src="assets/lib/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+  <script src="assets/lib/jquery.fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+  <script src="assets/js/app-pages-calendar.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      //-initialize the javascript
+      App.init();
+      App.pageCalendar();
+    });
+  </script>
+  <!-- <script>
     function loginuser() {
       let form = document.createElement('form')
       form.action = 'hello.php'
@@ -348,7 +350,7 @@ include("conexion.php");
       App.init();
       App.formElements();
     });
-  </script>
+  </script> -->
 </body>
 
 </html>

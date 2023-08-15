ec4aec5da07f461eb07c36899c79ee67efc57866
Lucas Ricci <lucric2001@gmail.com>
2023-08-15 11:16:24 -0300

    changed conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,12 @@
 <?php
-/*Datos de conexion a la base de datos remoto*/
-$db_host = "208.109.21.16"; // p3plzcpnl452763.prod.phx3.secureserver.net
-$db_user = "w3ezyugzcrtk";
-$db_pass = "Support1221!";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
 $db_name = "YouAirCharter";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(mysqli_connect_errno()){
-	echo 'No se pudo conectar a la base de datos : '.mysqli_connect_error();
+    echo 'No se pudo conectar a la base de datos : '.mysqli_connect_error();
 }
 session_start();

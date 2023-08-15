582c378ce122966a3e832bad1ea6e9ac7e6f5b57
Lucas Ricci <lucric2001@gmail.com>
2023-08-15 10:18:24 -0300

    merged testing branch

--- conexion.php
+++ conexion.php
@@ -1,13 +1,8 @@
 <?php
 /*Datos de conexion a la base de datos remoto*/
-// $db_host = "208.109.21.16"; // p3plzcpnl452763.prod.phx3.secureserver.net
-// $db_user = "w3ezyugzcrtk";
-// $db_pass = "Charter1221!";
-// $db_name = "YouAirCharter";
-
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_host = "208.109.21.16"; // p3plzcpnl452763.prod.phx3.secureserver.net
+$db_user = "w3ezyugzcrtk";
+$db_pass = "Charter1221!";
 $db_name = "YouAirCharter";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
